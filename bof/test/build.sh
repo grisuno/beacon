79a0312f769cda0d6efc1a1f50cc8677c7ec44fb
Grisuno <grisuno@gmail.com>
2025-09-20 00:05:22 -0300

    Update build.sh

--- bof/test/build.sh
+++ bof/test/build.sh
@@ -2,3 +2,4 @@
 x86_64-w64-mingw32-gcc -c -fPIC -O2 -fno-stack-protector Test.c -o test.x64.o
 x86_64-w64-mingw32-gcc -c -fPIC -O2 -fno-stack-protector winver.c -o winver.x64.o
 x86_64-w64-mingw32-gcc -c -fPIC -O2 -fno-stack-protector getenv.c -o getenv.x64.o
+x86_64-w64-mingw32-gcc -c -fPIC -O2 -fno-stack-protector cmdwhoami.c -o cmdwhoami.x64.o
