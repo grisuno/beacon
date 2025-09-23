8d10d42cf9ef5cf72586559972077748ef1d2eb5
Grisuno <grisuno@gmail.com>
2025-09-23 20:40:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 sudo apt install mingw-w64
 python3 app.py
+
+# Preguntar al usuario si quiere salir
+read -p "    [?] do you wannna install the C2 ? (y/n): " respuesta
+
+if [[ "$respuesta" == "n" || "$respuesta" == "N" ]]; then
+    echo "    [*] Happy Hacking ..."
+    exit 0
+else
+    git clone https://github.com/grisuno/LazyOwn.git
+
+fi
