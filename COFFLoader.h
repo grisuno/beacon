3380713fafcd78ecbafe049d06497c5a928dd847
Grisuno <grisuno@gmail.com>
2025-09-23 19:10:31 -0300

    Update COFFLoader.h

--- COFFLoader.h
+++ COFFLoader.h
@@ -1,3 +1,22 @@
+/*
+	This file is part of Black Basalt Beacon.
+
+	Black Basalt Beacon is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Black Basalt Beacon is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Black Basalt Beacon.  If not, see <https://www.gnu.org/licenses/>.
+
+	Copyright (c) LazyOwn RedTeam 2025. All rights reserved.
+*/
+
 #ifndef COFFLOADER_H
 #define COFFLOADER_H
 
