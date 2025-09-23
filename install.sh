cc22e3bfa7a81fd2c093d725958dd285b0ca9695
Grisuno <grisuno@gmail.com>
2025-09-23 20:44:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,20 @@
 sudo apt install mingw-w64
 python3 app.py
 
-# Preguntar al usuario si quiere salir
-read -p "    [?] do you wannna install the C2 ? (y/n): " respuesta
+# Espera 10 segundos por la respuesta del usuario
+read -t 10 -p "   [?] do you wanna install the C2 ? (y/n): " respuesta
 
-if [[ "$respuesta" == "n" || "$respuesta" == "N" ]]; then
-    echo "    [*] Happy Hacking ..."
-    exit 0
-else
-    git clone https://github.com/grisuno/LazyOwn.git
-
+# Si el usuario no respondió, la variable 'respuesta' estará vacía
+if [ -z "$respuesta" ]; then
+  # Asume la opción 'y' y procede con la instalación
+  respuesta="y"
+  echo
+  echo "Installing afer 10 seconds ..."
 fi
+
+# Ahora puedes usar la variable 'respuesta' para continuar
+if [ "$respuesta" = "y" ] || [ "$respuesta" = "Y" ]; then
+    echo "Installing the C2..."
+    git clone https://github.com/grisuno/LazyOwn.git
+fi
+echo "Happy hacking."
